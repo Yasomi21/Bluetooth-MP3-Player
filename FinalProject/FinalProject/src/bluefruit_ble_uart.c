ac25a3de36451113495b1f5e080b0ada29f1dfc2
Derrick <dlai3@ucsc.edu>
2025-03-10 03:33:26 -0700

    Tested out receive in custom UART, buggy but kinda works

--- FinalProject/FinalProject/src/bluefruit_ble_uart.c
+++ FinalProject/FinalProject/src/bluefruit_ble_uart.c
@@ -344,7 +344,7 @@ int main() {
     // Populate the transmit buffer
     uint8_t ch = 'A';
     for (int i = 0; i < (BUFFER_SIZE); i++) {
-        BLE_PutChar(ch + i);
+        BLE_PutChar(ch + i + 6);
         //set_leds(tx_buffer.tail);
         printf("Val: %d \n", tx_buffer.tail);
     }
@@ -353,6 +353,13 @@ int main() {
     while (TRUE) {
         BLE_RunLoop();
         //set_leds(tx_buffer.tail);
+
+        // If a character is present, print it out...?
+        uint8_t x;
+        uint8_t status = BLE_GetChar(&x);
+        if (status == SUCCESS) {
+            printf("Msg: %c\n", x); // UART is set to have just a newline ending, so don't need to put it there, '\n'
+        }
     }
 
     // while (TRUE) {
